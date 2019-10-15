b39506d5219b27fb060942c1c5b22ae7ea443fb5
federikos <federikos@bk.ru>
2019-10-15 04:49:51 +0300

    hide the list view on narrow screens

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IconSwitch from './IconSwitch';
 import CardsView from './CardsView';
 import ListView from './ListView';
-import cards from './data'
-// import ListView from './ListView'
+import cards from './data';
 
 export default () => {
   const [ icon, setIcon ] = useState('view_list');
+  const [ hideList, setHideList ] = useState(false);
+
+  //т.к. список хорош только для широких экранов, не будем отображать его на мобильных
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      let currentHideList = (window.innerWidth <= 700);
+      if (currentHideList !== hideList) {
+          setHideList(currentHideList);
+      }
+    });
+  });
+
+  useEffect(() => {
+    if(hideList) setIcon('view_list');
+  }, [hideList]);
+  //конец необязательного кода
 
   function onSwitch() {
     if (icon === 'view_module') {
@@ -18,7 +33,9 @@ export default () => {
 
   return (
     <>
-      <IconSwitch icon={icon} onSwitch={onSwitch} />
+      {
+        !hideList && <IconSwitch icon={icon} onSwitch={onSwitch} />
+      }
       {
         icon === 'view_list' && <CardsView cards={cards} />
       }
